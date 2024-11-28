18d2f73bbea0d8eb7a87310d5b7f22c57f5cbca5
motomark <markhawkins@blueyonder.co.uk>
2024-11-28 08:34:50 +0000

    Update vector_hashset.rs

--- vector_hashset.rs
+++ vector_hashset.rs
@@ -1,19 +1,17 @@
 use std::collections::HashSet;
 fn main() {
     
-    //let mut set: HashSet<String> = vec!["a".to_string(), "c".to_string(), "c".to_string()].into_iter().collect();
-
-    let mut ip_vec: Vec<String> = Vec::new();
-    ip_vec.push("192.168.0.1".to_string());
-    ip_vec.push("192.168.0.2".to_string());
-    ip_vec.push("192.168.0.3".to_string());
-    ip_vec.push("192.168.0.1".to_string());
+    // initialise and de-dupe all on one line.
+    let mut ip_vec = vec!["192.168.0.1".to_string(),
+            "192.168.0.2".to_string(),
+            "192.168.0.3".to_string(),
+            "192.168.0.1".to_string()]
+        .into_iter().collect::<HashSet<String>>()
+        .into_iter().collect::<Vec<String>>();
     
-    // Collect the Vector into a HashSet - then collect a deduped Vector.
-    let mut de_duped = ip_vec.into_iter().collect::<HashSet<String>>().into_iter().collect::<Vec<String>>();
-    de_duped.sort();
+    ip_vec.sort();
 
-     for item in de_duped {
+     for item in ip_vec {
         println!("Deduped Ip Address: {}", item);
     }
 
