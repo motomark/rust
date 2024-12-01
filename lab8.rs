c5608743b966c5aeb02c2858dfae426af80d172b
Mark H <markhawkins@blueyonder.co.uk>
2024-12-01 14:17:37 +0000

    lab8 extension task

--- lab8.rs
+++ lab8.rs
@@ -2,14 +2,38 @@
 fn main() {
     let mut str_input = String::new();
     std::io::stdin().read_line(&mut str_input).expect("Unable to read from stdin");
-    let pal = str_input.trim().to_lowercase().replace(" ", "").replace(",","").replace("!","");
-    let lap = pal.chars().rev().collect::<String>();
 
-    
-    if pal == lap  {
-        println!("Yes its a Palindrome");
+    let mut res : bool = is_palindrome(&str_input);
+    if res == true {
+        println!("Yes String {} is a Palindrome", str_input.trim());
     } else {
-        println!("No Palindrome");
+        println!("No String {} is not Palindrome", str_input.trim());
     }
 
+    let slice: &str = &str_input;
+    res = is_palindrome(slice);
+
+    if res == true {
+        println!("Yes Slice {} is a Palindrome", str_input.trim());
+    } else {
+      
+        println!("No Slice {} is not Palindrome", str_input.trim());
+    }
+
+
+}
+
+/// Test if palindrome. We only need to pass in a immutable string Slice to test.
+fn is_palindrome(input: &str) -> bool {
+
+    let pal = input.trim().to_lowercase().replace(" ", "").replace(",","").replace("!","");
+    let lap = pal.chars().rev().collect::<String>();
+
+    if pal == lap  {
+        return true;
+    } else {
+        return false;
+    }
+
+   
 }
